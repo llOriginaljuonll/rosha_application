6558ed98f8cdb40e1025c835475a5d2a25b4d5b7
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-06-05 08:42:24 +0700

    remove path url unnessary

--- core/urls.py
+++ core/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.competition.urls')),
-    path('participant/', include('apps.participant.urls')),
 ]
