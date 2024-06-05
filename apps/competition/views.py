ed371c62a0e73827b865598de624f6a7099fe385
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-06-05 14:42:10 +0700

    add formmixin in competitiondetailview

--- apps/competition/views.py
+++ apps/competition/views.py
@@ -1,7 +1,10 @@
 from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic.edit import FormMixin
+from django.http import HttpResponseForbidden
 from apps.competition.models import Competition
 from apps.competition.forms import CompetitionForm
-from django.urls import reverse_lazy
+from apps.participant.forms import ParticipantForm
+from django.urls import reverse_lazy, reverse
 
 class CompetitionCreateView(CreateView):
 
@@ -18,8 +21,30 @@ class CompetitionListView(ListView):
     template_name = 'competition/competition_list.html'
     context_object_name = 'competitions'
 
-class CompetitionDetailView(DetailView):
+class CompetitionDetailView(DetailView, FormMixin):
 
     model = Competition
-    template_name = 'competition/competition_detail.html'
+    form_class = ParticipantForm
+    template_name = 'participants/participant_form.html'
     context_object_name = 'competition'
+
+    def get_success_url(self) -> str:
+        return reverse_lazy('competition:list')
+    
+    def get_context_data(self, **kwargs):
+        context = super(CompetitionDetailView, self).get_context_data(**kwargs)
+        context['form'] = self.get_form()
+        return context
+    
+    def post(self, *args, **kwargs):
+        
+        form = self.get_form()
+        if form.is_valid():
+            form.save()
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+    
+    def form_valid(self, form):
+        return super().form_valid(form)
+    
