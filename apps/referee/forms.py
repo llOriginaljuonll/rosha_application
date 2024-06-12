d222455f2eac6d86d3006264dd85d28a54a175b3
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-06-12 12:31:53 +0700

    add logic about filling in values of foreignkey automatically

--- apps/referee/forms.py
+++ apps/referee/forms.py
@@ -6,3 +6,9 @@ class ScoreForm(forms.ModelForm):
     class Meta:
         model = Score
         fields = ('__all__')
+
+        widgets = {
+            'participant': forms.TimeInput(attrs={
+                'value': '', 'id': 'participant_id', 'type': 'hidden'
+            }),
+        }
