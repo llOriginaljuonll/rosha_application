4c1472a8416cb2456632e05b5e372e24cdb0ebed
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-06-14 13:00:37 +0700

    create func. for average score

--- apps/referee/models.py
+++ apps/referee/models.py
@@ -8,5 +8,10 @@ class Score(models.Model):
     rythm_score = models.IntegerField()
     perform_score = models.IntegerField()
 
+    def avg_score(self):
+        score = (self.skill_score, self.rythm_score, self.perform_score)
+        score_avg = sum(score) / len(score)
+        return score_avg
+
     def __str__(self):
         return f"No.{self.participant.id} {self.participant.name}"
