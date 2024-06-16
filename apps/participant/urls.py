2e72cce7bee2db4824f03909eb2b65b44621f411
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-06-17 03:20:38 +0700

    update urls

--- apps/participant/urls.py
+++ apps/participant/urls.py
@@ -4,8 +4,7 @@ from apps.participant import views
 app_name = 'participant'
 
 urlpatterns = [
-    path('<str:competition>', views.ParticipantListView.as_view(), name='list'),
-    path('participant_list_all/', views.ParticipantListAllView.as_view(), name='list-all'),
+    path('<str:pk>', views.ParticipantListView.as_view(), name='list'),
     path('participant_detail/<int:pk>', views.ParticipandDetailView.as_view(), name='detail'),
     path('participant_form/<pk>', views.ParticipantFormView.as_view(), name='form'),
 ]
--- apps/referee/urls.py
+++ apps/referee/urls.py
@@ -5,5 +5,4 @@ app_name = "referee"
 
 urlpatterns = [
     path('referee_form/<pk>', views.ScoreFormView.as_view(), name="form"),
-    path('result/', views.ScoreListView.as_view(), name="result"),
 ]
