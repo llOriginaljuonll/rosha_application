80e70d03e67847d720742aa2ca298eaa68d9b2bd
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-06-17 03:24:03 +0700

    edit Score model / add average field

--- apps/referee/models.py
+++ apps/referee/models.py
@@ -1,17 +1,25 @@
 from django.db import models
 from apps.participant.models import Participant
 
+
 class Score(models.Model):
 
-    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
-    skill_score = models.IntegerField()
-    rythm_score = models.IntegerField()
-    perform_score = models.IntegerField()
+    participant = models.OneToOneField(Participant, on_delete=models.CASCADE)
+    skill_score = models.DecimalField(max_digits=4, decimal_places=2)
+    rythm_score = models.DecimalField(max_digits=4, decimal_places=2)
+    perform_score = models.DecimalField(max_digits=4, decimal_places=2)
+    average = models.DecimalField(max_digits=4, decimal_places=2 ,null=True, blank=True)
 
     def avg_score(self):
-        score = (self.skill_score, self.rythm_score, self.perform_score)
-        score_avg = sum(score) / len(score)
-        return score_avg
+        list = (self.skill_score, self.rythm_score, self.perform_score)
+        result = sum(list) / len(list)
+        return result
+    
+    def save(self, *args, **kwargs):
+        if not self.average or self.average:
+            self.average = self.avg_score()
 
+        super().save(*args, **kwargs)
+    
     def __str__(self):
         return f"No.{self.participant.id} {self.participant.name}"
