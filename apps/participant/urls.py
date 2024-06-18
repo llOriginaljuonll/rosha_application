96c3e330f011e56fb22957fd531c4369949db586
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-06-18 17:06:52 +0700

    update participant url

--- apps/participant/urls.py
+++ apps/participant/urls.py
@@ -5,6 +5,7 @@ app_name = 'participant'
 
 urlpatterns = [
     path('<str:pk>', views.ParticipantListView.as_view(), name='list'),
-    path('participant_detail/<int:pk>', views.ParticipantDetailView.as_view(), name='detail'),
-    path('participant_form/<pk>', views.ParticipantFormView.as_view(), name='form'),
+    path('detail/<int:pk>', views.ParticipantDetailView.as_view(), name='detail'),
+    path('form/<pk>', views.ParticipantFormView.as_view(), name='form'),
+    path('update/<pk>', views.ParticipantUpdateView.as_view(), name='update')
 ]
