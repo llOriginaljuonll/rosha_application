810fb61d7fb5d3369e27ce66bcaa2a24737344e1
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-06-19 12:48:51 +0700

    remove score form page

--- apps/referee/urls.py
+++ apps/referee/urls.py
@@ -4,6 +4,6 @@ from apps.referee import views
 app_name = "referee"
 
 urlpatterns = [
-    path('referee_form/<pk>', views.ScoreFormView.as_view(), name="form"),
+    # path('referee_form/<pk>', views.ScoreFormView.as_view(), name="form"),
     path('update/<pk>', views.ScoreUpdateView.as_view(), name="update"),
 ]
