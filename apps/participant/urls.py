23ad8c0c968f33e280af7e3e547b99331c54730a
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-06-21 15:19:32 +0700

    create participant all list page

--- apps/participant/urls.py
+++ apps/participant/urls.py
@@ -4,6 +4,7 @@ from apps.participant import views
 app_name = 'participant'
 
 urlpatterns = [
+    path('list', views.AllParticipantLisview.as_view(), name='all-list'),
     path('<str:pk>', views.ParticipantListView.as_view(), name='list'),
     path('detail/<int:pk>', views.ParticipantDetailView.as_view(), name='detail'),
     path('form/<pk>', views.ParticipantFormView.as_view(), name='form'),
