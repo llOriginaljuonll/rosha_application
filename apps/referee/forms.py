ecdb96685b56909f6e35c4d2527a99c1dde7d777
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-06-26 16:01:23 +0700

    style score form page with crispy form

--- apps/referee/forms.py
+++ apps/referee/forms.py
@@ -1,14 +1,44 @@
 from django import forms
 from apps.referee.models import Score
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, Submit, HTML, Div, Field, ButtonHolder, LayoutObject
+from crispy_tailwind.layout import Button, Submit as Tailwind_Submit, Reset
+
+from crispy_forms.bootstrap import AppendedText, PrependedText, StrictButton
 
 class ScoreForm(forms.ModelForm):
 
     class Meta:
         model = Score
-        exclude = ('average',)
+        fields = '__all__'
+
+
 
         widgets = {
             'participant': forms.TextInput(attrs={
                 'value': '', 'id': 'participant_id', 'type': 'hidden',
             }),
         }
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        
+        self.helper = FormHelper()
+        self.helper.field_class = "bg-white"
+        self.helper.label_class = "content-center h-full border-e bg-gray-200 rounded-s-full rounded-b-none"
+        self.helper.render_required_fields = True
+        self.helper.layout = Layout(
+            Field(
+                "skill_score",
+                "rythm_score",
+                "perform_score",
+                css_class = "border-none text-center h-full rounded-none",
+                wrapper_class = "grid grid-cols-2 rounded-full text-base font-medium",
+                placeholder = "None"
+            ),
+            ButtonHolder(
+                Button("submit", "Submit")
+            ),
+        )
+
