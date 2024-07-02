79a2e74ba934ff75734bfb76dc282d0621d1e76b
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-07-02 11:48:10 +0700

    style score_update page

--- apps/referee/forms.py
+++ apps/referee/forms.py
@@ -1,44 +1,56 @@
 from django import forms
 from apps.referee.models import Score
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Fieldset, Submit, HTML, Div, Field, ButtonHolder, LayoutObject
-from crispy_tailwind.layout import Button, Submit as Tailwind_Submit, Reset
+from crispy_forms.layout import Layout, Submit, Field, ButtonHolder
 
-from crispy_forms.bootstrap import AppendedText, PrependedText, StrictButton
 
 class ScoreForm(forms.ModelForm):
+
+    # css = CSSContainer(
+    #     {"number": "bg-green-500"}
+    # )
+
+    skill_score = forms.NumberInput(attrs={"class": "bg-blue-500 text-red-300 here"})
 
     class Meta:
         model = Score
         fields = '__all__'
 
-
-
+        # skill_score = forms.CharField(
+        #     label='subject', 
+        #     max_length=100,
+        #     widget=forms.TextInput(
+        #         attrs={
+        #             'class': "form-control"
+        #         })
+        # )
         widgets = {
             'participant': forms.TextInput(attrs={
                 'value': '', 'id': 'participant_id', 'type': 'hidden',
             }),
         }
+    
+    # 'skill_score': forms.NumberInput(attrs={
+    #             "black": 'black',
+    #         })),
 
     def __init__(self, *args, **kwargs):
 
         super().__init__(*args, **kwargs)
-        
         self.helper = FormHelper()
-        self.helper.field_class = "bg-white"
-        self.helper.label_class = "content-center h-full border-e bg-gray-200 rounded-s-full rounded-b-none"
-        self.helper.render_required_fields = True
+        self.helper.label_class = "content-center h-full bg-gray-200 pb-0.5"
         self.helper.layout = Layout(
             Field(
+                "participant",
                 "skill_score",
                 "rythm_score",
                 "perform_score",
-                css_class = "border-none text-center h-full rounded-none",
-                wrapper_class = "grid grid-cols-2 rounded-full text-base font-medium",
-                placeholder = "None"
+                css_class = "text-center",
+                wrapper_class = "grid grid-cols-2 text-base font-medium",
+                placeholder = "None",
+                
             ),
             ButtonHolder(
-                Button("submit", "Submit")
+                Submit("submit", "Submit", css_class="text-xs md:text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer")
             ),
         )
-
