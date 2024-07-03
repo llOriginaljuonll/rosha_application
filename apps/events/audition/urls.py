e39adb024e5f90e9cefa43108610af307598972c
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-07-03 13:36:59 +0700

    rename url path

--- apps/events/audition/urls.py
+++ apps/events/audition/urls.py
@@ -5,7 +5,7 @@ app_name = 'audition'
 
 urlpatterns = [
     path('', views.AuditionListView.as_view(),name='list'),
-    path('competition_form/', views.AuditionCreateView.as_view(), name='form'),
-    path('competition_update/<pk>', views.AuditionUpdateView.as_view(), name="update"),
-    path('competition_delete/<pk>', views.AuditionDeleteView.as_view(), name="delete"),
+    path('audition_form/', views.AuditionCreateView.as_view(), name='form'),
+    path('audition_update/<pk>', views.AuditionUpdateView.as_view(), name="update"),
+    path('audition_delete/<pk>', views.AuditionDeleteView.as_view(), name="delete"),
 ]
