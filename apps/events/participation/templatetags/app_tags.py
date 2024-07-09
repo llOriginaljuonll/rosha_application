fbbb8cc4e819dac6086288fb8020e2cb6eb35317
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-07-09 12:38:08 +0700

    update my customize templatetags

--- apps/events/participation/templatetags/app_tags.py
+++ apps/events/participation/templatetags/app_tags.py
@@ -4,5 +4,5 @@ register = template.Library()
 
 def replace_audition(value):
     value = str(value).lower()
-    return value.replace("audition", "")
+    return value.replace("audition", "").replace("[]", "").strip()
 register.filter("replace_audition", replace_audition)
