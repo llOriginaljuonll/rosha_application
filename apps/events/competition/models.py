ff726792a5b5523dabe1b51789abfde21ccd0cf1
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-07-20 20:52:30 +0700

    create competition form page

--- apps/events/competition/models.py
+++ apps/events/competition/models.py
@@ -4,16 +4,24 @@ from versatileimagefield.fields import VersatileImageField
 
 class Competition(models.Model):
 
+    """
+        auto_now : When set to True, the current timestamp will be saved every time the object is saved.
+        editable : If set to False, this field will not be displayed in the admin form.
+    """
+
     name = models.CharField(max_length=255)
     category = models.CharField(max_length=255, default="competition")
-    comp_poster = VersatileImageField()
+    compt_poster = VersatileImageField()
     email = models.EmailField(default='rosha.thailand@gmail.com')
-    elig = models.JSONField(max_length=255)
-    instr_type = models.JSONField(max_length=255)
-    deadline = models.DateTimeField(default=date.today, null=True, blank=True)
-    ann_date = models.DateTimeField(default=date.today, null=True, blank=True)
-    compet_date = models.DateTimeField(default=date.today, null=True, blank=True)
-    descr_payment = models.CharField(max_length=255, null=True, blank=True)
+    elig = models.JSONField(blank=True, null=True)
+    instr_type = models.JSONField(blank=True, null=True)
+    deadline = models.DateField(null=True, blank=True)
+    deadline_time = models.TextField(null=True, blank=True, editable=False)
+    ann_date = models.DateField(null=True, blank=True)
+    ann_time = models.TextField(null=True, blank=True, editable=False)
+    compt_date = models.DateField(null=True, blank=True)
+    compt_time = models.TimeField(null=True, blank=True, editable=False)
+    descr_payment = models.TextField(max_length=255, null=True, blank=True)
     create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- apps/events/competition/views.py
+++ apps/events/competition/views.py
@@ -3,22 +3,22 @@ from django.urls import reverse_lazy
 from django.views.generic import FormView
 from .forms import CompetitionForm
 
-class CompetitionFormtView(FormView):
+class CompetitionFormView(FormView):
     template_name = 'events/competition/comp_form.html'
     form_class = CompetitionForm
     success_url = reverse_lazy('audition:form')
 
     def form_valid(self, form):
-        elig_objs = self.request.POST.getlist("elig") 
+        elig_objs = self.request.POST.getlist("eligible") 
         elig = {
             "elig": elig_objs,
         }
-        intr_types = self.request.POST.getlist("type")
-        intr_type = {
-            "type": intr_types
+        instr_objs = self.request.POST.getlist("type")
+        instr_types = {
+            "type": instr_objs,
         }
         competition = form.save(commit=False)
         competition.elig = elig
-        competition.intr_type = intr_type
+        competition.instr_type = instr_types
         competition.save()
         return super().form_valid(form)
