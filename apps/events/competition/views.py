8f927e4373370c95dfe78d5661204baa95ba6e6f
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-07-23 01:49:10 +0700

    add competition in overview page / navbar

--- apps/events/competition/views.py
+++ apps/events/competition/views.py
@@ -6,7 +6,7 @@ from .forms import CompetitionForm
 class CompetitionFormView(FormView):
     template_name = 'events/competition/comp_form.html'
     form_class = CompetitionForm
-    success_url = reverse_lazy('audition:form')
+    success_url = reverse_lazy('audition:list')
 
     def form_valid(self, form):
         elig_objs = self.request.POST.getlist("eligible") 
