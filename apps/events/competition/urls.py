898e1a35cb06632caa83999a0d31f6f50cbff2da
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-07-23 15:23:34 +0700

    create list page / update page of competition

--- apps/events/competition/urls.py
+++ apps/events/competition/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from apps.events.competition.views import ComptFormView, ComptListView
+from apps.events.competition.views import ComptFormView, ComptListView, ComptUpdateView, ComptDeleteView
 
 app_name="compt"
 
 urlpatterns = [
-    path('form/', CompetitionFormView.as_view(), name='form'),
+    path('form/', ComptFormView.as_view(), name='form'),
+    path('list/', ComptListView.as_view(), name='list'),
+    path('update/<pk>', ComptUpdateView.as_view(), name='update'),
+    path('delete/<pk>', ComptDeleteView.as_view(), name='delete'),
 ]
