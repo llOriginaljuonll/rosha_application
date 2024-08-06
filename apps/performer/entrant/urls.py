00759ff371f78f384f7171d6031a6f4c2275e9e9
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-08-06 13:22:34 +0700

    create update entrant page

--- apps/performer/entrant/urls.py
+++ apps/performer/entrant/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import EntrantFormView, EntrantListView, EntrantDetailView
+from .views import EntrantFormView, EntrantListView, EntrantDetailView, EntrantUpdateView
 
 app_name="entrant"
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('form/<pk>', EntrantFormView.as_view(), name="form"),
     path('<str:pk>', EntrantListView.as_view(), name="list"),
     path('detail/<pk>/', EntrantDetailView.as_view(), name="detail"),
+    path('update/<pk>/', EntrantUpdateView.as_view(), name="update"),
 ]
