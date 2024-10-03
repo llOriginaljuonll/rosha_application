993f6851304467cad5aa0637dec0d0c7eff6350e
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-10-03 09:32:33 +0700

    add support for DateTimeField when applying date_input_attrs to fields

--- core/forms.py
+++ core/forms.py
@@ -7,6 +7,13 @@ class BaseModelForm(forms.ModelForm):
 
         # Loop through all fields and apply date_input_attrs to DateField widgets
         for field_name, field in self.fields.items():
-            if isinstance(field, forms.DateField):
+            if isinstance(field, (forms.DateField, forms.DateTimeField)):  # รองรับทั้ง DateField และ DateTimeField
                 field.widget = forms.TextInput(attrs={})
                 field.widget.attrs.update(date_input_attrs)
+
+
+        # Loop through all fields to apply placeholders
+        if hasattr(self.Meta, 'placeholders'):
+            for field_name, placeholder in self.Meta.placeholders.items():
+                if field_name in self.fields:
+                    self.fields[field_name].widget.attrs.update({'placeholder': placeholder})
