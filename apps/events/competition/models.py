8541f1cf0a4836fee40d29db54aa3637d439f95b
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-10-30 14:11:58 +0700

    add field fee, field foreignkey hall model with all events model

--- apps/events/competition/models.py
+++ apps/events/competition/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from datetime import date
 from versatileimagefield.fields import VersatileImageField
+from apps.hall.models import Hall
 
 class Competition(models.Model):
 
     """
         auto_now : When set to True, the current timestamp will be saved every time the object is saved.
     """
+
+    hall = models.ForeignKey(Hall, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=255)
     category = models.CharField(max_length=255, default="competition")
     compt_poster = VersatileImageField()
@@ -19,7 +22,7 @@ class Competition(models.Model):
     ann_time = models.TimeField(null=True, blank=True)
     compt_date = models.DateField(null=True, blank=True)
     compt_time = models.TimeField(null=True, blank=True)
-    descr_payment = models.TextField(max_length=255, null=True, blank=True)
+    fee = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     create_at = models.DateTimeField(auto_now_add=True)
 
     def get_days_left(self, target_date):
