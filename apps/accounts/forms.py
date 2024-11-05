053c70f515e014d38cdd4f4d2bfaf6ba8e489215
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-11-05 10:09:12 +0700

    add field user_type in model CustomUser

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,13 +1,34 @@
 from allauth.account.forms import SignupForm, LoginForm
 from crispy_forms.helper import FormHelper
+from django import forms
+from .models import CustomUser
 
 class UserSignupForm(SignupForm):
+
+    USER_TYPE_CHOICES = [
+        ('', '--------'),
+        ('self', 'self'),
+        ('teacher', 'teacher'),
+        ('parent', 'parent'),
+    ]
+
+    user_type = forms.ChoiceField(
+        choices=USER_TYPE_CHOICES,
+        label="Select your user type", 
+        initial=''
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
+        self.helper.label_class = "font-normal bg-rose-300"
         self.fields["email"].label = "Email"
-        self.fields['password1'].help_text = None
+
+    def save(self, request):
+        user = super(UserSignupForm, self).save(request)
+        user.user_type = self.cleaned_data['user_type']
+        user.save()
+        return user
 
 class UserLoginForm(LoginForm):
 
--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,13 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class CustomUser(AbstractUser):
 
+class CustomUser(AbstractUser):
+    
     id: int
     email = models.EmailField("Email Address", unique=True, blank=False, null=False)
     first_name = None
     last_name = None
     is_judge = models.BooleanField("Judge Status", default=False, help_text="Designates whether the user can grades auditioners.")
+    user_type = models.CharField(max_length=100, default="")
 
     def __str__(self):
-        return f'{self.username} No.{self.id}'
+        return f"{self.username} No.{self.id}"
