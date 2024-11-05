bc9057abaf399a93c6a8c328695e623e92a968f1
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-11-05 10:00:49 +0700

    reconfigure all style files
    uninstall @tailwindcss/typography package, rename main tailwind file name main.css, edit and add scripts tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit', // เปิดใช้งานโหมด JIT
-  darkMode: 'selector',
   content: [
-    './templates/**/*.html',
-    './node_modules/flowbite/**/*.js',
-    './src/**/*.{js,jsx,ts,tsx}', // เพิ่มเส้นทางไฟล์สำหรับโหมด JIT
-    './public/index.html', // เพิ่มเส้นทางไฟล์สำหรับโหมด JIT
+    "./templates/**/",
+    "./**/*.py",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {
-      padding: {
-        '12': '3rem', // ตรวจสอบว่ามีการกำหนดค่า padding นี้
-      },
       screens: {
         'sm': '300px',
         'md': '640px',
@@ -23,14 +17,6 @@ module.exports = {
     },
   },
   plugins: [
-    require('flowbite/plugin'),
-    function ({ addUtilities }) {
-      const newUtilities = {
-        '.placeholder-bold::placeholder': {
-          fontWeight: '300',
-        },
-      };
-      addUtilities(newUtilities, ['responsive', 'hover', 'focus']);
-    },
+    require('flowbite/plugin')
   ],
 }
