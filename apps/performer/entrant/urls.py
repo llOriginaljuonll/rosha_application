d655b1f26678afd10fd33488d527b3c2edbbbd50
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-12-11 15:56:40 +0700

    correct importing module for entrant url page

--- apps/performer/entrant/urls.py
+++ apps/performer/entrant/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import EntrantFormView, EntrantListView, EntrantDetailView, EntrantDeleteView
+from .views import EntrantCreateView, EntrantListView, EntrantDetailView, EntrantDeleteView
 
 app_name="entrant"
 
 urlpatterns = [
-    path('entrant_form/<int:competition_id>/', EntrantFormView.as_view(), name="form"),
+    path('entrant_form/<int:competition_id>/', EntrantCreateView.as_view(), name="form"),
     path('<str:pk>', EntrantListView.as_view(), name="list"),
     path('detail/<pk>/', EntrantDetailView.as_view(), name="detail"),
     path('delete/<pk>/', EntrantDeleteView.as_view(), name="delete"),
